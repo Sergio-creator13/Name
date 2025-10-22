f31529529cb2f4a95ea4f25918f645d113f00a74
Sergio-creator13 <sergioamolina083@gmail.com>
2025-10-22 22:44:53 +0000

    me

---
+++ shop.java
@@ -0,0 +1,11 @@
+public class shop{
+    public static void main(String args[]){
+        System.out.println(23+34);
+        double greed = 53*35;
+        greed = greed/89;
+        System.out.println(greed);
+        String fred = "sam";
+        System.out.println(fred);
+
+    }
+}
